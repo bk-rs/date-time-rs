f07ef2b2466a82e0c2bda50fe75865a6387d3a7e
vkill <vkill.net@gmail.com>
2021-09-15 15:56:02 +0800

    Update

--- date-range/src/lib.rs
+++ date-range/src/lib.rs
@@ -1,7 +1,7 @@
 use core::fmt;
 use std::error;
 
-use chrono::{Duration, NaiveDate, NaiveDateTime};
+use chrono::{Duration, NaiveDate, NaiveDateTime, Utc};
 
 mod iter;
 pub use iter::DateRangeIterator;
@@ -15,6 +15,43 @@ impl DateRange {
     pub fn new(since: NaiveDate, until: NaiveDate) -> Result<Self, Error> {
         if since > until {
             return Err(Error::SinceShouldLtUntil);
+        }
+
+        Ok(Self {
+            since_: since,
+            until_: until,
+        })
+    }
+    pub fn from_timestamp(
+        since: i64,
+        mut until: i64,
+        configuration: Option<DateRangeFromTimestampConfiguration>,
+    ) -> Result<Self, Error> {
+        let configuration = configuration.unwrap_or_default();
+
+        if since > until {
+            return Err(Error::SinceShouldLtUntil);
+        }
+
+        if configuration.now_gteq_until {
+            let now = Utc::now().timestamp();
+            if until > now {
+                until = now;
+
+                if since > until {
+                    return Err(Error::SinceShouldLtUntil);
+                }
+            }
+        }
+
+        let since = NaiveDateTime::from_timestamp(since, 0);
+        let until = NaiveDateTime::from_timestamp(until, 0);
+
+        let mut since = since.date();
+        let until = until.date();
+
+        if (until - since).num_days() > configuration.max_interval.num_days() {
+            since = until - configuration.max_interval;
         }
 
         Ok(Self {
@@ -55,6 +92,28 @@ impl DateRange {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct DateRangeFromTimestampConfiguration {
+    pub now_gteq_until: bool,
+    pub max_interval: Duration,
+}
+impl Default for DateRangeFromTimestampConfiguration {
+    fn default() -> Self {
+        Self {
+            now_gteq_until: true,
+            max_interval: Duration::days(2 * 365), // 2 years
+        }
+    }
+}
+impl DateRangeFromTimestampConfiguration {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+//
+//
+//
 #[derive(Debug, Clone)]
 pub struct DateTimeRange {
     since_: NaiveDateTime,
@@ -117,7 +176,7 @@ mod tests {
     use std::error;
 
     #[test]
-    fn test_new() -> Result<(), Box<dyn error::Error>> {
+    fn test_date_range_new() -> Result<(), Box<dyn error::Error>> {
         match DateRange::new("2021-08-02".parse().unwrap(), "2021-08-02".parse().unwrap()) {
             Ok(_) => {}
             Err(err) => {
@@ -133,7 +192,59 @@ mod tests {
     }
 
     #[test]
-    fn test_since_datetime_and_until_datetime() {
+    fn test_date_range_from_timestamp() {
+        let now = Utc::now();
+
+        //
+        let date_range = DateRange::from_timestamp(
+            (now - Duration::days(7)).timestamp(),
+            (now + Duration::days(3)).timestamp(),
+            None,
+        )
+        .unwrap();
+        assert_eq!(
+            date_range.since(),
+            now.naive_utc().date() - Duration::days(7)
+        );
+        assert_eq!(date_range.until(), now.naive_utc().date());
+
+        //
+        let mut configuration = DateRangeFromTimestampConfiguration::new();
+        configuration.now_gteq_until = false;
+        let date_range = DateRange::from_timestamp(
+            (now - Duration::days(7)).timestamp(),
+            (now + Duration::days(3)).timestamp(),
+            Some(configuration),
+        )
+        .unwrap();
+        assert_eq!(
+            date_range.since(),
+            now.naive_utc().date() - Duration::days(7)
+        );
+        assert_eq!(
+            date_range.until(),
+            now.naive_utc().date() + Duration::days(3)
+        );
+
+        //
+        let date_range = DateRange::from_timestamp(
+            (now - Duration::days(10)).timestamp(),
+            now.timestamp(),
+            Some(DateRangeFromTimestampConfiguration {
+                now_gteq_until: true,
+                max_interval: Duration::days(7),
+            }),
+        )
+        .unwrap();
+        assert_eq!(
+            date_range.since(),
+            now.naive_utc().date() - Duration::days(7)
+        );
+        assert_eq!(date_range.until(), now.naive_utc().date());
+    }
+
+    #[test]
+    fn test_date_range_since_datetime_and_until_datetime() {
         let date_range =
             DateRange::new("2021-08-09".parse().unwrap(), "2021-08-15".parse().unwrap()).unwrap();
 
@@ -148,7 +259,7 @@ mod tests {
     }
 
     #[test]
-    fn test_prev_and_next() {
+    fn test_date_range_prev_and_next() {
         let date_range =
             DateRange::new("2021-08-09".parse().unwrap(), "2021-08-15".parse().unwrap()).unwrap();
 
